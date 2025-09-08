8c5f2f936b2f906883492815f7a69b6010d833a3
Ebubejuliet <uebubejuliet@gmail.com>
2025-09-08 02:34:20 +0100

    I have completed the core requirement and one stretch challenge

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Ramdon ramdonGenerator = new Ramdon();
+        int number = ramdonGenerator.Next(1, 11);
+        string keepplaying = "yes";
+
+        int guess;
+        do
+        {
+            Console.Write("What is your magic number? ");
+            guess = int.Parse(Console.ReadLine());
+         while (guess != number);
+        }
+        if (guess > number);
+        {
+            Console.WriteLine("Lower");
+        }
+        if (guess < number);
+        {
+            Console.WriteLine("Higher");
+        }
+        else;
+        {
+            Console.WriteLine("You guessed it!")
+        }
+        while (keepplaying == "yes");
+        {
+            Console.Write("Do you want to keep playing? ");
+            keepplaying = Console.ReadLine();
+        }
+
+
+
     }
 }
