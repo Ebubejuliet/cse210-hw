6e0692c3cab3b65a230ff8bbd53d7a9e040aae65
Ebubejuliet <uebubejuliet@gmail.com>
2025-09-08 01:14:13 +0100

    I added the required code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,6 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
-        Console.Write("What is your first name");
+        Console.Write("What is your first name? ");
+        string firstname = Console.Readline();
+        Console.Write("What is your last name? ");
+        string lastname = Console.Readline();
+        Console. WriteLine($"Your name is {lastname}, {firstname} {lastname}")
     }
 }
