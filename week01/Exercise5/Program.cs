5ae949d9001f2b072180a6c7b0b41094e2d8ac95
Ebubejuliet <uebubejuliet@gmail.com>
2025-09-09 02:27:39 +0100

    I have added requirement

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(userNumber);
+
+        DisplayResult(userName, squaredNumber);
+
+    }
+    static void DisplayWelcomeMessage();
+    {
+        
+        Console.WriteLine("Welcome to the program!");
+    }
+    static void PromptUserName();
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLiine();
+        return userName;
+    }
+    static void PromptUserNumber();
+    {
+        Console.Write("Please enter your favorite number: ");
+        int userNumber = Console.ReadLiine();
+        return userNumber;
+    }
+    static void squaredNumber(int userNumber);
+    {
+        int SquareNumber = userNumber * userNumber;
+        return SquareNumber;
+    }
+    static void DisplayResult(string userName, int SquareNumber);
+    {
+        Console.WriteLine($"{userName}, the square of your number is {squaredNumber}");
     }
 }
